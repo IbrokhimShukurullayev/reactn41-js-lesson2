9f15049c57ecad648ff62bebaa546baab3051717
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2023-12-10 23:22:57 +0500

    boolen

---
+++ js/main.js
@@ -0,0 +1,256 @@
+//   Boolean1
+
+// let A = 2;
+
+// let  res = A % 2 == 0;
+
+// console.log(res);
+
+// Boolean2
+
+// let A = 3;
+// let B = -6;
+
+// let res = A >= 0;
+// let res1 = B < -2;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean3
+
+// let A = 3;
+// let B = 5;
+// let C = 7;
+
+// let res = A < B < C;
+
+// console.log(res);
+
+// Boolean4
+
+// let A = 5;
+// let B = 7;
+
+// let res = A % 2 == 1;
+// let res1 = B % 2 == 1;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean5
+
+// let A = 5;
+// let B = 8;
+
+// let res = A % 2 !== 0;
+// let res1 = B % 2 !== 0;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean6
+
+// let A = 13;
+// let B = 32;
+
+// let res = A % 2 !== 0;
+// let res1 = B % 2 !== 0;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean7
+
+
+// let A = 3;
+// let B = 5;
+// let C = 7;
+
+// let res = A > 0;
+// let res1 = B > 0;
+// let res2 = C > 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// Boolean8
+
+// let A = 3;
+// let B = -11;
+// let C = 13;
+
+// let res = A < 0;
+// let res1 = B < 0;
+// let res2 = C < 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+
+// Boolean9
+
+// let A = -12;
+// let B = 43;
+// let C = -456;
+
+// let res = A > 0;
+// let res1 = B > 0;
+// let res2 = C > 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// Boolean10
+
+// let A = 12;
+
+// let res = (A % 100) % 2 == 0;
+
+// console.log(res);
+
+
+// Boolean11
+
+// let A = 121;
+
+// let res = (A % 1000) % 2 == 1;
+
+// console.log(res);
+
+// Operators1
+
+// let A = 3;
+// let B = 10;
+
+// let Y = (-B / 2) * Math.pow(A, 2) + Math.pow(B, 2);
+
+// console.log(Y);
+
+// Operators2
+
+// let A = 4;
+// let B = 5;
+// let C = 6;
+// let D = 7;
+
+// let Y = Math.pow(A + B / C + D), 2) + Math.pow(B, 2);
+
+// console.log(Y);
+
+// Operators3
+
+// let A = 5;
+// let C = 7;
+// let D = 9;
+
+// let Y = 2 * Math.pow(A, D) + 4 * Math.pow(C, 2) / 3;
+
+// console.log(Y);
+
+// Operators4
+
+// let A = 5;
+// let B = 6;
+// let C = 7;
+// let D = 9;
+
+// let Y = 1 / Math.pow(A , 2) * (B / 10)  3 * (C + D)  2;
+
+// console.log(Y);
+
+// Operators5
+
+// let A = 9;
+// let B = 4;
+// let C = 8;
+// let D = 6;
+
+// let Y = Math.pow((((A / B) - 1) / (C / (D - 1))) ,2);
+
+// console.log(Y);
+
+// Operators6
+
+// let A = 3;
+// let B = 7;
+// let C = 5;
+// let D = 8;
+
+// let Y = (A + B) / (C + D) / (A + C);
+
+// console.log(Y);
+
+// Operators7
+
+// let A = 3;
+// let B = 4;
+// let C = 8;
+
+// let Y = Math.sqrt(Math.pow(A + B + C, 2) - Math.pow(A - B - C, 2));
+
+// console.log(Y);
+
+// Operators8
+
+// let A = 2;
+// let B = 3;
+// let C = 5;
+
+// let Y = ((Math.pow(A , 2) + Math.pow(B , 2) + Math.pow(C , 2)) / (A * B * C));
+
+// console.log(Y);
+
+// Operators9
+
+// let A = 2;
+// let B = 3;
+// let C = 4;
+// let D = 5;
+
+// let Y = A * (Math.pow(D, 3) / 3) + B * (Math.pow(C, 2) / 2);
+
+// console.log(Y);
+
+// Operators10
+
+// let A = 3;
+// let B = 5;
+// let C = 7;
+
+// let Y = Math.abs(A - B + B - C) + Math.pow((A + C), 1 / 4);
+
+// console.log(Y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
